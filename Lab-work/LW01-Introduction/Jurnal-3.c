159d4d8a8519449144a23db7dc53fe5070445a4e
Tanto Wijaya <tanto.twjy@gmail.com>
2021-09-30 10:06:56 +0700

    Add labwork 1

---
+++ Lab-work/LW01-Introduction/Jurnal-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+  printf("Nama : Tanto Wijaya \n");
+  printf("NIM  : 1102213211 \n");
+  printf("Saya siap mengikuti praktikum algoritma dan pemrograman.");
+
+  return 0;
+
+}
---
+++ Lab-work/LW01-Introduction/TP-6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  //deklarasi variabel
+  float a,b;
+
+  //input
+  printf("a = ");
+  scanf("%f", &a);
+  printf("b = ");
+  scanf("%f", &b);
+
+  //output
+  printf("Hasil dari a/b adalah %.2f", a/b);
+
+  return 0;
+}
