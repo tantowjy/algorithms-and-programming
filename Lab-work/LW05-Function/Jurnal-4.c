fd0c7b73480fcd3f0e57f98e2676b15b7ea5ef28
Tanto Wijaya <tanto.twjy@gmail.com>
2021-10-28 09:12:22 +0700

    Add labwork 5

---
+++ Lab-work/LW05-Function/Jurnal-4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int i;
+  float c, f, r;
+  printf("Suhu (C) = ");
+  scanf("%f", &c);
+  printf("Menu Konversi : \n");
+  printf("1. Reamur\n");
+  printf("2. Farenheit\n");
+  printf("Pilih : ");
+  scanf("%d", &i);
+
+  switch (i)
+  {
+    case 1:
+      r = c*4/5;
+      printf("Hasil Konversi : %.2f R", r);
+      break;
+    case 2:
+      f = (c*9/5)+32;
+      printf("Hasil konversi : %.2f F", f);
+      break;
+    default:
+      printf("Menu tidak tersedia");
+      break;
+  }
+
+  return 0;
+}
